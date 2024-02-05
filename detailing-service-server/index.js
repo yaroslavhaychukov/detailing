ca6baa1a2a0318067a85b82a24ecc50ce23c85cf
yaroslavhaychukov <138558630+yaroslavhaychukov@users.noreply.github.com>
2024-02-05 21:36:59 +0300

    feat: initial server files

---
+++ detailing-service-server/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const sequelize = require("./db");
+const PORT = process.env.PORT || 5000;
+const models = require('./models/models.js')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(fileUpload({}))
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use('/api', router)
+
+
+app.use(errorHandler)
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
